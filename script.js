377786e945b38f3380c81cf55782307b39186133
aokoye <aokoye@gmail.com>
2025-03-23 13:51:57 -0700

    Draft calculator logic

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ let subtract = (a, b) => (a) - (b);
 let multiply = (a, b) => (a) * (b);
 let divide = (a, b) => (a) / (b);
 
+let operate = function(a, op, b) {
+    let subTotal = 0
+    if (op === '+') {
+        a = add(a, b)
+    } else if (op === '-') {
+        a = subtract(a, b)
+    } else if (op === '*') {
+        a = multiply(a, b)
+    } else if (b === 0 && op === '/') {
+        return 'Absolutely not.'
+    }
+     else {
+        a = divide(a, b)
+    }
+
+    console.log(a)
+}
+
+// console.log(operate(5, '/', 2))
+
 // console.log(add(5, 8))
 // console.log(subtract(10, 7))
